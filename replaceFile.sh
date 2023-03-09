4b46ed991ae636587c6ab5cf58865fb52916e6c0
peacekeeper228 <sasa.osipov.01@yandex.ru>
2023-03-09 21:33:22 +0300

    file for Linux was accomplished

--- replaceFile.sh
+++ replaceFile.sh
@@ -5,7 +5,9 @@ if [ ! -d schoolMIPS/scripts/program/common/ ]; then
     exit 1;
 fi
 
+#updating compiling files
 cat << EOF > schoolMIPS/scripts/program/common/02_compile_to_hex_with_mars.sh 
+#!/bin/bash
 java -jar ..\..\scripts\bin\Mars4_5.jar nc a dump .text HexText program.hex main.S
 if [ ! -f main1.S  ]; then
     echo "Assembly file for 2 core is not found and therefore is not compiled!. It should be called main1.S"
@@ -14,4 +16,15 @@ else
 fi
 EOF
 
-echo 'copy .\program1.hex ..\..\board\program' >> schoolMIPS/scripts/program/common/05_copy_program_to_board.sh 
+cat << EOF > schoolMIPS/scripts/program/common/02_compile_to_hex_with_mars.bat 
+java -jar ..\..\scripts\bin\Mars4_5.jar nc a dump .text HexText program.hex main.S
+IF EXIST "main1.S" (
+  java -jar ..\..\scripts\bin\Mars4_5.jar nc a dump .text HexText program1.hex main1.S
+) ELSE (
+  ECHO "Assembly file for 2 core is not found and therefore is not compiled!. It should be called main1.S"
+)
+EOF
+
+#updating coping files
+echo 'copy .\program1.hex ..\..\board\program' >> schoolMIPS/scripts/program/common/05_copy_program_to_board.bat 
+echo 'cp .\program1.hex ..\..\board\program' >> schoolMIPS/scripts/program/common/05_copy_program_to_board.sh 
